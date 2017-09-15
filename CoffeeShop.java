c5dc75d34a2ebc239ffc58bc106f253108505d9b
Hoyin Yip <hoyin_yip@m013477.aw.org>
2017-09-14 20:41:52 -0700

    The CoffeeShop is ready

---
+++ CoffeeShop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class CoffeeShop{
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    String a =in.nextLine();
+    System.out.println("Welcome to Yip Coffee!~");
+
+    Scanner in1= new Scanner(System.in);
+    String b = in1.nextLine();
+    System.out.println("Sure. Tall, grande, or venti?");
+
+    Scanner in2= new Scanner(System.in);
+    String c = in2.nextLine();
+    System.out.println("Got it! Can I have Your name please?");
+
+    Scanner in3= new Scanner(System.in);
+    String d = in3.nextLine();
+    System.out.println("Thank you. Here is your coffee~");
+  }
+}
