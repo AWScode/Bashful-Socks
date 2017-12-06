532952c68328eed4c05ffad1e24cc109babf715b
Hoyin Yip <hoyin_yip@m013477.aw.org>
2017-12-06 13:50:16 -0800

    HI

---
+++ Binary/Binary-1.java
@@ -0,0 +1,26 @@
+//Vivian Yip
+//2017 12 06
+
+*/
+import java.util.*;
+
+public class ShoppingSpree {
+
+  ArrayList<int> FirstList = new ArrayList<string>
+
+public static void main(String[] args) {
+
+  System.out.println("Input first binary number:");
+
+  Scanner first = new Scanner(System.in);
+  int a = Integer.parseInt(first.nextLine());
+
+  System.out.println("Input second binary number:");
+
+  Scanner second = new Scanner(System.in);
+  int b = Integer.parseInt(second.nextLine());
+
+System.out.println("Choose add, subtract, multiply, divide");
+
+  }
+}
--- Binary/Binary.java
+++ Binary/Binary.java
@@ -1,2 +1,26 @@
 //Vivian Yip
 //2017 12 06
+
+*/
+import java.util.*;
+
+public class Binary{
+
+
+  public static void main(String[] args) {
+    ArrayList<int> FirstList = new ArrayList<string>
+
+    System.out.println("Input first binary number:");
+
+    Scanner first = new Scanner(System.in);
+    int a = Integer.parseInt(first.nextLine());
+
+    System.out.println("Input second binary number:");
+
+    Scanner second = new Scanner(System.in);
+    int b = Integer.parseInt(second.nextLine());
+
+    System.out.println("Choose add, subtract, multiply, divide");
+
+  }
+}
