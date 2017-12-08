9316e5fee79afc22ace11dc42dac445c41b5854e
Hoyin Yip <hoyin_yip@m013477.aw.org>
2017-12-08 13:49:30 -0800

    HI

--- Binary/Binary.java
+++ Binary/Binary.java
@@ -6,21 +6,53 @@ import java.util.*;
 
 public class Binary{
 
+public String add(){
+
+}
 
   public static void main(String[] args) {
-    ArrayList<int> FirstList = new ArrayList<string>
+    Binary calculator = new calculator();
 
-    System.out.println("Input first binary number:");
+    System.out.println("Input first binary number:(please type one nubmer at a time, and press enter after every number)");
+
+
+    int [] int1Array = new int [7];
 
     Scanner first = new Scanner(System.in);
-    int a = Integer.parseInt(first.nextLine());
+    int 1 = parseInt(first.nextLine());
+    int a = intArray1
+
+    Scanner first = new Scanner(System.in);
+    int 2 = parseInt(first.nextLine());
+
+    Scanner first = new Scanner(System.in);
+    int 3 = parseInt(first.nextLine());
+
+    Scanner first = new Scanner(System.in);
+    int 4 = parseInt(first.nextLine());
+
+    Scanner first = new Scanner(System.in);
+    int 5 = parseInt(first.nextLine());
+
+    Scanner first = new Scanner(System.in);
+    int 6 = parseInt(first.nextLine());
+
+    Scanner first = new Scanner(System.in);
+    int 7 = parseInt(first.nextLine());
+
+    Scanner first = new Scanner(System.in);
+    int 8 = parseInt(first.nextLine());
 
     System.out.println("Input second binary number:");
 
     Scanner second = new Scanner(System.in);
-    int b = Integer.parseInt(second.nextLine());
+    int b = parseInt(second.nextLine());
 
     System.out.println("Choose add, subtract, multiply, divide");
+
+    Scanner function = new Scanner(System.in);
+    char operator = parseInt(first.nextLine());
+
 
   }
 }
