0f8985c9e49b492af8bd651eb0495b5edc14ea20
Hoyin Yip <hoyin_yip@m013477.aw.org>
2017-12-10 22:09:25 -0800

    HI

--- Binary/Binary.java
+++ Binary/Binary.java
@@ -16,32 +16,23 @@ public String add(){
     System.out.println("Input first binary number:(please type one nubmer at a time, and press enter after every number)");
 
 
-    int [] int1Array = new int [7];
+    int a = new int [8];
+    int b = new int [8];
+    int f = new int [8];
+
+    for (int i= 0; i < 8; i++){
+      char tempChar = number1.charAt(i);
+      data1[i] = Character.getNumericValue(tempChar);
+    }
+
+    for (int i= 0; i < 8; i++){
+      System.out.print(data1[i]);
+    }
+    System.out.print("no");
 
     Scanner first = new Scanner(System.in);
     int 1 = parseInt(first.nextLine());
-    int a = intArray1
 
-    Scanner first = new Scanner(System.in);
-    int 2 = parseInt(first.nextLine());
-
-    Scanner first = new Scanner(System.in);
-    int 3 = parseInt(first.nextLine());
-
-    Scanner first = new Scanner(System.in);
-    int 4 = parseInt(first.nextLine());
-
-    Scanner first = new Scanner(System.in);
-    int 5 = parseInt(first.nextLine());
-
-    Scanner first = new Scanner(System.in);
-    int 6 = parseInt(first.nextLine());
-
-    Scanner first = new Scanner(System.in);
-    int 7 = parseInt(first.nextLine());
-
-    Scanner first = new Scanner(System.in);
-    int 8 = parseInt(first.nextLine());
 
     System.out.println("Input second binary number:");
 
@@ -53,6 +44,36 @@ public String add(){
     Scanner function = new Scanner(System.in);
     char operator = parseInt(first.nextLine());
 
+// method
 
+public static addition (){
+  if (a [0].equal(0) && b [0].equal(0)){
+    f[0]= 0;
+    else if ((a[0].equal(1) && b[0].equal(0)) || (a[0].equal(0) && b[0].equal(1))
+    f[0] =0;
+    remainder =1;
+    else f[0]=1
+  }
+  if (a [1].equal(0) && b [1].equal(0)){
+    f[1]= 0;
+    else if ((a[1].equal(1) && b[1].equal(0)) || (a[1].equal(0) && b[1].equal(1))
+    f[1] =0;
+    remainder =1;
+    else f[1]=1
+}
+  if (a [2].equal(0) && b [2].equal(0)){
+    f[2]= 0;
+    else if ((a[2].equal(1) && b[2].equal(0)) || (a[2].equal(0) && b[2].equal(1))
+    f[2] =0;
+    remainder =1;
+    else f[2]=1
+  }
+  if (a [3].equal(0) && b [3].equal(0)){
+    f[3]= 0;
+    else if ((a[3].equal(1) && b[3].equal(0)) || (a[3].equal(0) && b[3].equal(1))
+    f[3] =0;
+    remainder =1;
+    else f[3]=1
+  }
   }
 }
