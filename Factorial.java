3426e5be39f2b848e7a27084990d0af5086831f3
Hoyin Yip <hoyin_yip@m013477.aw.org>
2018-01-31 14:06:20 -0800

    Hi

--- Factorial.java
+++ Factorial.java
@@ -2,14 +2,21 @@
 code how to calculate the factorial of the number 10
 and print it
 */
+import java.util.*;
 
 public class Factorial{
+  long fac;
+  long num;
   public static void main(String[] args) {
-    int fac;
-    fac = 10;
-    for (int i=10; i>=1; i--){
-      fac *= i;
-      System.out.println(fac);
+    System.out.print("Please enter a number");
+    Scanner input = new Scanner(System.in);
+    long fac = input.nextLong();
+    System.out.println(fac);
+  }
+  public static long V(String[] args) {
+
+    for (long i=num; i>=1; i--){
+      long fac = long num * i;
       }
     }
   }
