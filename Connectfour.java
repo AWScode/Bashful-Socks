4d3bd34397db2750f735cca4aea19b7ca1438a0d
Hoyin Yip <hoyin_yip@m013477.aw.org>
2018-03-13 17:27:38 -0700

    almost

--- Connectfour.java
+++ Connectfour.java
@@ -21,32 +21,36 @@ public class Connectfour {
 
   Boolean gameOver;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) {//main method
     Connectfour newGame = new Connectfour();
-    newGame.displayBoard();
+    newGame.displayBoard();//runs displayBoard method
     //newGame.checkFour();
-    String player = "2";
+    String player = "2"; //player = 2
     while (true) {
-      if (player.equals("1")) {
-        player = "2";
+      if (player.equals("1")) { //if player is 1
+        player = "2"; //then player actually equals 2
       }
       else {
-        player = "1";
+        player = "1"; //otherwise player = 1
       }
       System.out.println("Player " + player + "s turn.");
 
-      Scanner newSc = new Scanner(System.in);
-      newGame.displayBoard();
+      Scanner newSc = new Scanner(System.in);//gethers user's input
+      newGame.displayBoard();//displays board in whatever form (original or new)
       System.out.println("Where do you want to play?");
-      int col = newSc.nextInt()-1;
+      int col = newSc.nextInt()-1; //user's input minus 1 = the column number used for addPiece
       //-1 in order to let 1 be the first column
-      newGame.addPiece(col, player);
-      newGame.checkFourUR(player);
-      newGame.checkFourR(player);
-      
-      newGame.checkFourD(player);
+      newGame.addPiece(col, player); //runs method and sets col and player as parameters for addPiece method
+      newGame.checkFour();//runs the different checkFour methods
+      Boolean end = newGame.checkFour();
+      if (end == true) {
+        System.out.println("Game Over! ");
+        System.out.println("Player " + player + " wins!");
+        break;
+      }
 
-    }
+
+    } //end of while loop
   }
 
     //newGame.addPiece(col, player);
@@ -122,34 +126,34 @@ public class Connectfour {
     this.board[6] = column7;
 
     this.gameOver = false;
-  }
+  } //data for the game
 
   // Get and Set Methods here
-  public void getPiece(){
+  public void getPiece(){//unused method
 
 
   }
 
   // Other methods here
-  public void displayBoard(){
-    for (int j = 0; j < 6; j++) {
+  public void displayBoard(){//method to display the board
+    for (int j = 0; j < 6; j++) {//j is row
       String row = "";
-      for (int i = 0; i < 7; i++) {
-        row = row + board[i][j] + " ";
+      for (int i = 0; i < 7; i++) {//i is column
+        row = row + board[i][j] + " ";//know what is happenning but not really how
       }
-      System.out.println(row);
+      System.out.println(row);//prints the board
     }
   }
 
-  public void addPiece(int col, String player) {
+  public void addPiece(int col, String player) {//method to add a piece. user and column determined in main
 
-    for(int j =5; j>-1; j--){
-     if (board[col][j].equals ("0")){
-       board[col][j] = player;
-       break;
+    for(int j =5; j>-1; j--){//loops through rows
+     if (board[col][j].equals ("0")){//if the space is empty...
+       board[col][j] = player;//then replace 0 with whatever player it is (either 1 or 2)
+       break;//stop loop
      }
-     else if (board[col][0].equals ("1") || board[col][0].equals ("2")){
-       System.out.println("-----------Not here-----------");
+     else if (board[col][0].equals ("1") || board[col][0].equals ("2")){//otherwise, if the space is full...
+       System.out.println("-----------Not here-----------");//it means the column is full so this will be outputted
        //if the person misplaced it in a column that is already full, she/he will not have another chance to put another piece
        break;
      }
@@ -158,89 +162,206 @@ public class Connectfour {
 
 
 
- public void checkFourUR(String player){
-   int c = 0;
-   for(int i =0; i<7; i++){
-     for (int j = 0; j<6; j++){
-       if (board[i][j].equals("1")){
-       for (int k=1; k<5; k++){
+ public Boolean checkFour(){//method that checks for 4-in-a-rows going in the NW direction
+   int c = 0;//c will represent the in-a-rows
+   for(int i =0; i<7; i++){//looping through columns
+     for (int j = 0; j<6; j++){//and looping through rows
+       if (board[i][j].equals("1")){//once found a 1 on the board (the first one found)
+         c = 1;
 
-       if (board[i+k][j-k].equals ("1")){
-         k = k+1;
-         c++;
-         System.out.println(c);
+       for (int k=1; k<4; k++){
+//UR
+         if (j-k > -1){
+           if (i+k < 7){
+
+       if (board[i+k][j-k].equals ("0")){//then will look in the NW direction for another 1
+
+         c = 1;//not sure what this does
+         break;//stop the loop
      }
-     else if (board[i+k][j-k].equals ("0") || board[i+k][j-k].equals ("2")){
+     else if (board[i+k][j-k].equals ("2")){
+       c = 1;
        break;
      }
-}
-if (c == 2){
-System.out.println("Player 1 won UR");
-System.out.println("press control + c");
-break;
 
-
-}
-
-}
-}
-break;
-}
-}
-
-public void checkFourR(String player){
-  int c = 0;
-  for(int i =0; i<7; i++){
-    for (int j = 0; j<6; j++){
-      if (board[i][j].equals("1")){
-      for (int k=0; k<4; k++){
-        /*if (!board[col+k][j+k].equals ("1")){
-          return false;
-      }*/
-      if (board[i+k][j].equals ("1")){
-        k = k+1;
-        c = c+1;
-    }
-    if (c == 6){
-    System.out.println("Player 1 won R");
-    System.out.println("press control + c");
-
-  }
-
-    break;
-}
-}
-}
-}
+     else {
+       c++;
+       if (c == 4){//when it count to four
+         gameOver = true;//the player wins the game
+         break;
+       }
+     }
+   }
+ }
 }
 
 
-public void checkFourD(String player){
-  int c = 0;
-  for(int i =0; i<7; i++){
-    for (int j = 0; j<6; j++){
-      if (board[i][j].equals("1")){
-      for (int k=0; k<3; k++){
-        /*if (!board[col+k][j+k].equals ("1")){
-          return false;
-      }*/
-      if (board[i][j+k].equals ("1")){
-        k = k+1;
-        c = c+1;
+c = 1;
+          for (int k = 1; k < 4; k++) {
+            //right
+            if (i + k < 7) {
+              if (board[i+k][j].equals("0")) {
+                c = 1;
+                break;
+              }
+              else if (board[i+k][j].equals("2")) {
+                c = 1;
+                break;
+              }
+              else {
+                c++;
+                if (c == 4){
+                  gameOver = true;
+                  break;
+                }
+              }
+
+            }
+          }
+
+          c = 1;
+          for (int k = 1; k < 4; k++) {
+            //bottom-right
+            if (j+k < 6) {
+              if (i + k < 7) {
+                if (board[i+k][j+k].equals("0")) {
+                  c = 1;
+                  break;
+                }
+                else if (board[i+k][j+k].equals("2")) {
+                  c = 1;
+                  break;
+                }
+                else {
+                  c++;
+                  if (c == 4){
+                    gameOver = true;
+                    break;
+                  }
+                }
+              }
+            }
+          }
+          c = 1;
+          for (int k = 1; k < 4; k++) {
+            //bottom
+            if (j+k < 6) {
+              if (board[i][j+k].equals("0")) {
+                c = 1;
+                break;
+              }
+              else if (board[i][j+k].equals("2")) {
+                c = 1;
+                break;
+              }
+              else {
+                c++;
+                if (c == 4){
+                  gameOver = true;
+                  break;
+                }
+              }
+            }
+          }
+        }
+
+
+        if (board[i][j].equals("2")) {
+          c = 1;
+          for (int k = 1; k < 4; k++) {
+            //up-right
+            if (j-k > -1) {
+              if (i + k < 7) {
+                if (board[i+k][j-k].equals("0")) {
+                  c = 1;
+                  break;
+                }
+                else if (board[i+k][j-k].equals("1")) {
+                  c = 1;
+                  break;
+                }
+                else {
+                  c++;
+                  if (c == 4){
+                    gameOver = true;
+                    break;
+                  }
+                }
+              }
+            }
+          }
+          c = 1;
+          for (int k = 1; k < 4; k++) {
+            //right
+            if (i + k < 7) {
+              if (board[i+k][j].equals("0")) {
+                c = 1;
+                break;
+              }
+              else if (board[i+k][j].equals("1")) {
+                c = 1;
+                break;
+              }
+              else {
+                c++;
+                if (c == 4){
+                  gameOver = true;
+                  break;
+                }
+              }
+
+            }
+          }
+
+          c = 1;
+          for (int k = 1; k < 4; k++) {
+            //bottom-right
+            if (j+k < 6) {
+              if (i + k < 7) {
+                if (board[i+k][j+k].equals("0")) {
+                  c = 1;
+                  break;
+                }
+                else if (board[i+k][j+k].equals("1")) {
+                  c = 1;
+                  break;
+                }
+                else {
+                  c++;
+                  if (c == 4){
+                    gameOver = true;
+                    break;
+                  }
+                }
+              }
+            }
+          }
+          c = 1;
+          for (int k = 1; k < 4; k++) {
+            //bottom
+            if (j+k < 6) {
+              if (board[i][j+k].equals("0")) {
+                c = 1;
+                break;
+              }
+              else if (board[i][j+k].equals("1")) {
+                c = 1;
+                break;
+              }
+              else {
+                c++;
+                if (c == 4){
+                  gameOver = true;
+                  break;
+                }
+              }
+            }
+          }
+        }
       }
-      if (c == 4){
-      System.out.println("Player 1 won D");
-      System.out.println("press control + c");
-
     }
-
-    break;
-}
-}
-}
-}
-}
-
+    return gameOver;
+  }
 
 
 
